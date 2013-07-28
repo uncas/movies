12a84296fb1e881182e07e5d61dbf1af876abd77
Ole Lynge Soerensen <olelynge@gmail.com>
2013-07-28 08:08:32 +0200

    Add single-page MVC template.

---
+++ src/Uncas.Movies.Web/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Uncas.Movies.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+    }
+}
---
+++ src/Uncas.Movies.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Uncas.Movies.Web.MvcApplication" Language="C#" %>
