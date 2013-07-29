1b7fd56aaeedc0fe2edc9decf0b5c9d1f6c0c775
Ole Lynge Soerensen <olelynge@gmail.com>
2013-07-29 11:30:54 +0200

    Add simple background job.

---
+++ src/Uncas.Movies.Web/App_Start/JobSetup.cs
@@ -0,0 +1,32 @@
+﻿using Uncas.Movies.Web;
+using Uncas.Movies.Web.Jobs;
+using WebActivator;
+using WebBackgrounder;
+
+[assembly: PostApplicationStartMethod(typeof (JobSetup), "Start")]
+[assembly: ApplicationShutdownMethod(typeof (JobSetup), "Shutdown")]
+
+namespace Uncas.Movies.Web
+{
+    public static class JobSetup
+    {
+        private static readonly JobManager JobManager = CreateJobWorkersManager();
+
+        public static void Start()
+        {
+            JobManager.Start();
+        }
+
+        public static void Shutdown()
+        {
+            JobManager.Dispose();
+        }
+
+        private static JobManager CreateJobWorkersManager()
+        {
+            var jobs = new[] {new MovieJob()};
+            var coordinator = new SingleServerJobCoordinator();
+            return new JobManager(jobs, coordinator);
+        }
+    }
+}
---
+++ src/Uncas.Movies.Web/Jobs/MovieJob.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using WebBackgrounder;
+
+namespace Uncas.Movies.Web.Jobs
+{
+    public class MovieJob : Job
+    {
+        public MovieJob() : base("MovieJob", TimeSpan.FromMinutes(60d))
+        {
+        }
+
+        public override Task Execute()
+        {
+            return new Task(Work);
+        }
+
+        private static void Work()
+        {
+            Thread.Sleep(TimeSpan.FromSeconds(1d));
+        }
+    }
+}
