cc31ba9d46e83c8f278573dd8afd6c4f46f074b4
Ole Lynge Soerensen <olelynge@gmail.com>
2013-07-31 13:41:52 +0200

    Crawl 'East of Eden'.

---
+++ tests/Uncas.Movies.Tests/CinemaCrawlerTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Uncas.Movies.Tests
+{
+    [TestFixture]
+    public class CinemaCrawlerTests
+    {
+        private async Task<string> CrawlAsync(string url)
+        {
+            using (var client = new HttpClient())
+            {
+                HttpResponseMessage message = await client.GetAsync(url);
+                return await message.Content.ReadAsStringAsync();
+            }
+        }
+
+        private string Crawl(string url)
+        {
+            Task<string> task = CrawlAsync(url);
+            task.Wait();
+            return task.Result;
+        }
+
+        [Test]
+        public void CrawlEastOfEden()
+        {
+            string message = Crawl("http://www.paradisbio.dk/program.asp?bio=aarhusc");
+            Console.WriteLine(message);
+        }
+    }
+}
